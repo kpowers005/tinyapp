3829a3393a5ead6b0be41b74ff404e1bd63cb787
Kyle <k.powers005@gmail.com>
2021-09-14 17:45:04 -0400

    getting there?

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+let carousel = document.getElementById('carousel')
+
+let forward = document.getElementById('forwardArrow')
+let back = document.getElementById('backArrow')
+
+forward.addEventListener('click', ())
+})
