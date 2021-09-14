90b1009de856c17d0f17ddd57e01f8750ce5df58
Kyle <k.powers005@gmail.com>
2021-09-14 17:50:49 -0400

    rotation?

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  let links = ['app', 'blog', 'shop', 'note', 'game', 'grave']
 let carousel = document.getElementById('carousel')
+carousel.innerText = links[0]
 
 let forward = document.getElementById('forwardArrow')
 let back = document.getElementById('backArrow')
+forward.addEventListener('click', () => {
+  let rotate = links.shift()
+  carousel.innerText = links[0]
+  links.push(rotate)
+});
 
-forward.addEventListener('click', ())
+back.addEventListener('click', () => {
+  let rotate = links.pop()
+  links.unshift(rotate)
+  carousel.innerText = links[0]
+})
 })
