48f264dd4a3e1ed462c32ca42372888a2a3688c2
Kyle <k.powers005@gmail.com>
2021-09-16 16:28:32 -0400

    tiny grave fix

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  let links = ['app', 'blog', 'shop', 'note', 'game', 'grave']
-  let carousel = document.getElementById('carousel')
+  const links = ['app', 'blog', 'shop', 'note', 'game', 'grave']
+  const carousel = document.getElementById('carousel')
+  const body = document.getElementById('body')
+  const comingSoon = document.querySelector('.coming-soon')
   carousel.innerText = links[0]
 
   let forward = document.getElementById('forwardArrow')
   let back = document.getElementById('backArrow')
-  forward.addEventListener('click', () => {
+
+  const goDark = () => {
+    carousel.style.color = 'black';
+    body.style.backgroundColor = 'grey';
+    body.style.color = 'white';
+    forward.style.color = 'white'
+    back.style.color = 'white'
+    comingSoon.innerText = 'r.i.p.'
+  }
+
+  const goLight = () => {
+    carousel.style.color = '#f03e56';
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+    forward.style.color = 'black'
+    back.style.color = 'black'
+    comingSoon.innerText = 'coming soon'
+  }
+
+  forward.onclick = () => {
     let rotate = links.shift()
     carousel.innerText = links[0]
+    links[0] === 'grave' ? goDark() : goLight()
     links.push(rotate)
-  });
+  };
 
-  back.addEventListener('click', () => {
+  back.onclick = () => {
     let rotate = links.pop()
-    if (rotate === 'grave') {
-      carousel.classList.add('grave')
-      let body = document.getElementById('body')
-      body.classList.add('black')
-    }
     links.unshift(rotate)
+
+    links[0] === 'grave' ? goDark() : goLight()
+
     carousel.innerText = links[0]
-  })
+  }
+
 })
